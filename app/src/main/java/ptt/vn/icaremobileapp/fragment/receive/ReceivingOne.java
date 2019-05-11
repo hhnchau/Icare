5e4eb108f8a7d82934f2822d69cdd0ac86411607
Chau Huynh Ngoc <chauhn@phuthotech.vn>
2019-05-11 12:07:00 +0700

    Change Utils Add Fragment + Add Fragment Receiving

---
+++ app/src/main/java/ptt/vn/icaremobileapp/fragment/receive/ReceivingOne.java
@@ -0,0 +1,51 @@
+package ptt.vn.icaremobileapp.fragment.receive;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ptt.vn.icaremobileapp.BaseFragment;
+import ptt.vn.icaremobileapp.R;
+import ptt.vn.icaremobileapp.fragmentutils.Directionez;
+import ptt.vn.icaremobileapp.fragmentutils.Fragmentez;
+import ptt.vn.icaremobileapp.fragmentutils.Fragmentoz;
+import ptt.vn.icaremobileapp.fragmentutils.Fragmentuz;
+
+public class ReceivingOne extends BaseFragment {
+    private View view;
+    private List<Fragmentoz> lstFragment = new ArrayList<>();
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.receiving_one, container, false);
+
+        view.findViewById(R.id.go).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (getActivity() != null) {
+                    FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                    Fragmentuz.addFragment(lstFragment, fragmentManager, Fragmentez.RECEIVING_TWO, false, R.id.frameReceiving, null, Directionez.NEXT);
+                }
+            }
+        });
+
+        view.findViewById(R.id.back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (getActivity() != null) {
+                    //FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                    //Fragmentuz.addFragment(lstFragment, fragmentManager, Fragmentez.RECEIVING_ONE, R.id.frameReceiving, null, Directionez.NEXT);
+                }
+            }
+        });
+
+        return view;
+    }
+}
